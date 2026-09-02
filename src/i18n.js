2ec1ccd6ae8ee6ad4db8e9f0e5f95f10eb73b047
Maicol <mdgonzalez4124@gmail.com>
2026-09-02 18:34:36 -0500

    feat: add i18n for text and parser xml

---
+++ src/i18n.js
@@ -0,0 +1,13 @@
+import i18next from 'i18next';
+import es from './locales/es.js';
+import en from './locales/en.js';
+
+i18next.init({
+  lng: 'en',
+  resources: {
+    es,
+    en,
+  },
+});
+
+export default i18next;
---
+++ src/locales/es.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    title: 'Agregador de RSS',
+    form: {
+      label: 'Enlace RSS',
+      submit: 'Añadir',
+    },
+    errors: {
+      required: 'No puede estar vacío',
+      url: 'El enlace debe ser una URL válida',
+      duplicate: 'El enlace ya ha sido agregado',
+    },
+  },
+};
